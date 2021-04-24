13dbabf063e0ac7dcb3088fb32a0499ab4c15d0a
MuhammadAichee <muhammad.aichee@gmail.com>
2021-04-24 22:05:11 +0500

    Working

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -1,1 +1,22 @@
 import React, { Component } from 'react';
+import { GoogleLogin } from 'react-google-login';
+class Login extends Component {
+	// responseGoogle = (response) => {
+    //     console.log(response);
+    // }
+    ahsan = () =>{
+
+    }
+    render() {
+		return (
+			<GoogleLogin
+				clientId={'473267098191-7k89d77kv37ngntp6oc5le7o95u2m294.apps.googleusercontent.com'}
+				// onSuccess={this.responseGoogle}
+				// onFailure={this.responseGoogle}
+			>
+				{/* <FontAwesome name="google" /> */}
+				<span> Login with Google</span>
+			</GoogleLogin>
+		);
+	}
+}
