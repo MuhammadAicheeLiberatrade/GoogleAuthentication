dd62852c445ef1adcfc5ed6fd6a937a63471cc05
MuhammadAichee <muhammad.aichee@gmail.com>
2021-04-24 23:24:49 +0500

    Working on logout

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -1,24 +1,40 @@
 import React, { Component } from 'react';
 import { GoogleLogin } from 'react-google-login';
-export class Login extends Component {
-	responseGoogle = (response:any) => {
-        console.log(response);
+import { Redirect } from 'react-router';
+type MyState = {
+	loginCorrect: boolean; // like this
+};
+export class Login extends Component<MyState> {
+	state: MyState = {
+		loginCorrect: false
+	};
 
-        var res = response.profileObj;
+	responseGoogle = (response: any) => {
+		console.log(response, 'Ahsan');
 
-        console.log(res);
+		var res = response.profileObj;
 
-        debugger;	}
+		console.log(res);
+		localStorage.setItem('Data', JSON.stringify(res));
+		this.setState({
+            loginCorrect:true
+        })
+        debugger;
+	};
 	ahsan = () => {};
 	render() {
-		const responseGoogle = (response:any) => {
-			
-		};
+		const clientId: string = '473267098191-7k89d77kv37ngntp6oc5le7o95u2m294.apps.googleusercontent.com';
+		if (this.state.loginCorrect === true) {
+			return <Redirect to="/main" />;
+		}
 		return (
 			<GoogleLogin
-				clientId={'473267098191-7k89d77kv37ngntp6oc5le7o95u2m294.apps.googleusercontent.com'}
+				clientId={clientId}
+				cookiePolicy={'single_host_origin'}
 				onSuccess={this.responseGoogle}
 				onFailure={this.responseGoogle}
+				buttonText="Login with Google"
+				style={{ marginTop: '1000px', backgroundColor: 'blue' }}
 			>
 				{/* <FontAwesome name="google" /> */}
 				<span> Login with Google</span>
--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -1,11 +1,50 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Redirect } from 'react-router';
+import { GoogleLogout } from 'react-google-login';
 
-export default class Main extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+type MyState = {
+	UserData: any;
+};
+export default class Main extends Component<MyState> {
+	state: MyState = {
+		UserData: {
+			email: '',
+			familyName: '',
+			givenName: '',
+			imageUrl: '',
+			name: ''
+		}
+	};
+	componentDidMount() {
+		var Data = JSON.parse(localStorage.getItem('Data') || '{}');
+		this.setState({
+			UserData: Data
+		});
+	}
+	render() {
+		const UserData = this.state.UserData;
+        const clientId: string = '473267098191-7k89d77kv37ngntp6oc5le7o95u2m294.apps.googleusercontent.com';
+		if (UserData !== {})
+			return (
+				<div>
+					{this.state.UserData !== {} ? (
+						<header className="App-header">
+							<img src={UserData.imageUrl} alt="logo" />
+							<p>{UserData.email}</p>
+							<a className="App-link" target="_blank" rel="noopener noreferrer">
+								{UserData.name}
+							</a>
+							<GoogleLogout
+								clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+								buttonText="Logout"
+								// onLogoutSuccess={logout}
+							/>
+						</header>
+					) : null}
+				</div>
+			);
+		else {
+			return <Redirect to="/" />;
+		}
+	}
 }
